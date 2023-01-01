45313ed73c3611e43801caf94782148aeed6461a
devIS <ivan.shaptalov1@gmail.com>
2023-01-01 15:55:24 +0200

    feat(main.dart): created new page with liked titles

--- lib/main.dart
+++ lib/main.dart
@@ -10,62 +10,88 @@ class RandomWords extends StatefulWidget {
 
   @override
   State<RandomWords> createState() => _RandomWordsState();
-  }
+}
 
-  class _RandomWordsState extends State<RandomWords> {
-    final _suggestions = <WordPair>[];
-    final _saved = <WordPair>{};
-    final _biggerFont = const TextStyle(fontSize: 18);
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
 
-    @override
-    Widget build(BuildContext context) {
-      return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return const Divider();
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(tiles: tiles, context: context).toList()
+          : <Widget>[];
 
-          final index = i ~/ 2;
-
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-            
-          }
-
-          final alreadySaved = _saved.contains(_suggestions[index]);
-          return ListTile(
-            title: Text(_suggestions[index].asPascalCase,
-            style: _biggerFont),
-            trailing: Icon(
-              alreadySaved? Icons.favorite : Icons.favorite_border,
-              color: alreadySaved? Colors.red : null,
-              semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-            ),
-            // add logic
-            onTap:() => setState(() {
-              alreadySaved? _saved.remove(_suggestions[index]) : _saved.add(_suggestions[index]);
-            })
-          );
-        } 
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Saved Suggestions'),
+        ),
+        body: ListView(children: divided),
       );
-    }
-
-
+    }));
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+          actions: [
+            IconButton(
+              onPressed: _pushSaved,
+              icon: const Icon(Icons.list),
+              tooltip: 'Saved Suggestions',
+            )
+          ],
+        ),
+        body: ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: /*1*/ (context, i) {
+              if (i.isOdd) return const Divider();
+
+              final index = i ~/ 2;
+
+              if (index >= _suggestions.length) {
+                _suggestions.addAll(generateWordPairs().take(10));
+              }
+
+              final alreadySaved = _saved.contains(_suggestions[index]);
+              return ListTile(
+                trailing: Icon(
+                  alreadySaved ? Icons.favorite : Icons.favorite_border,
+                  color: alreadySaved ? Colors.red : null,
+                  semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+                ),
+                // add logic
+                onTap: () => setState(() {
+                  alreadySaved
+                      ? _saved.remove(_suggestions[index])
+                      : _saved.add(_suggestions[index]);
+                }),
+                title:
+                    Text(_suggestions[index].asPascalCase, style: _biggerFont),
+              );
+            }));
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Startup Name Generator",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Name Generator'),
-        ),
-        body: const Center(child: RandomWords()),
-      ),
+      home: RandomWords(),
     );
   }
 }
